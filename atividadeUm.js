01568a902a7a1cddfc4e61b15e8f2cbc4e2eee5b
Augusta <vi_augusta@usp.br>
2022-10-12 00:46:50 -0300

    Atividade 01

---
+++ atividadeUm.js
@@ -0,0 +1,10 @@
+let distanciaEmQuilometros = 89.3
+let tempoEmHoras = 1.33
+const distancia = distanciaEmQuilometros
+const tempo = tempoEmHoras
+const velocidadeMedia = distanciaEmQuilometros / tempoEmHoras
+const velocidadeMediaDuasCasas = velocidadeMedia.toFixed(2);
+
+
+console.log (` A velocidade média é ${velocidadeMediaDuasCasas} km/h `)
+
